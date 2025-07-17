145edf54ce9197a34a77ad8b82d739558c32b597
Felipe Dornelles <lipe.dornelles@hotmail.com>
2025-07-17 11:52:05 -0300

    feat: create get ratings endpoint

---
+++ src/pages/api/ratings/index.api.ts
@@ -0,0 +1,20 @@
+import { prisma } from "@/lib/prisma";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== "GET") {
+    return res.status(405).end();
+  }
+
+  const ratings = await prisma.rating.findMany({
+    orderBy: {
+      created_at: "desc",
+    },
+    include: {
+      user: true,
+      book: true,
+    },
+  });
+
+  return res.json(ratings);
+}
